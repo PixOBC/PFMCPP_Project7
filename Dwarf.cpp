bba92467320bd9085ed4775ddfd4a23742883740
James Pickering <james@oneblankchannel.com>
2021-06-07 20:21:40 +0100

    Updating Dwarf class

--- Dwarf.cpp
+++ Dwarf.cpp
@@ -1,7 +1,18 @@
 #include "Dwarf.h"
 
-//Dwarf::Dwarf
+Dwarf::Dwarf(std::string name, int hp_, int armor_)
+    : Character(hp_, armor_, 4)
+{
 
-//Dwarf::getName
+}
+
+const std::string& Dwarf::getName()
+{
+    return name;
+}
+std::string Dwarf::getStats()
+{
+    return getCharacterStats(this);
+}
 
 //Dwarf::getStats
--- Dwarf.h
+++ Dwarf.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "Character.h"
+#include "Utility.h"
 
 struct Dwarf : Character
 {
+    Dwarf(std::string name, int hp_, int armor_);
+
+    const std::string& getName() override;
+    std::string getStats() override;
+
 private:
     const std::string name;
 };
